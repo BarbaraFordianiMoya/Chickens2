f0d07fe7437db31fc224fc6063b22e5490a2d815
Aluno <Aluno@10.85.198.10>
2024-06-12 14:55:51 -0300

    Chickens2

---
+++ src/Chickens02.java
@@ -0,0 +1,16 @@
+public class Chickens02 {
+    public static void main(String[] args) {
+        //Put yout code here
+        int x=100;
+        int y=121;
+        int z=117;
+        double w=(x+y+z)/3.00;
+        double v=w*30;
+        double u=v*0.18;
+        
+        System.out.println(w);
+        System.out.println(v);
+        System.out.println(u);
+    }
+    
+}
